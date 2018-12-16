5d03448df66e59e0ea06f63d300d75c65bf1c780
bingyuhuang <huangbingyu2018@gmail.com>
2018-12-16 21:52:23 +0800

    combat:add two number by java

--- knowledge/algorithms/combat/code/java/AddTwoNumbers.java
+++ knowledge/algorithms/combat/code/java/AddTwoNumbers.java
@@ -1,3 +1,23 @@
 public class AddTwoNumbers {
-	
+	public static void addTwoNumber(List<Integer> num1, List<Integer> num2) {
+
+        LinkedList<Integer> result = new LinkedList<Integer>();
+        int i = num1.size();
+        int j = num2.size();
+        int overflow = 0;
+
+        while (true) {
+            if (i > 0) {
+                overflow = overflow + num1.get(--i);
+            }
+            if (j > 0) {
+                overflow = overflow + num2.get(--j);
+            }
+            result.addFirst(overflow % 10);
+            overflow = overflow / 10;
+            if (i == 0 && j == 0 && overflow == 0) {
+                break;
+            }
+        }
+    }
 }
