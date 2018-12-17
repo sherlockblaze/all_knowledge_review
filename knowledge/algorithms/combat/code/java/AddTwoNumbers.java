c255683a07cab2b438505badb7a3bf9e5c783f9f
bingyuhuang <huangbingyu2018@gmail.com>
2018-12-17 23:44:06 +0800

    modify algorithm:LinkList to List

--- knowledge/algorithms/combat/code/java/AddTwoNumbers.java
+++ knowledge/algorithms/combat/code/java/AddTwoNumbers.java
@@ -1,7 +1,7 @@
 public class AddTwoNumbers {
 	public static void addTwoNumber(List<Integer> num1, List<Integer> num2) {
 
-        LinkedList<Integer> result = new LinkedList<Integer>();
+        List<Integer> result = new LinkedList<Integer>();
         int i = num1.size();
         int j = num2.size();
         int overflow = 0;
@@ -13,7 +13,7 @@ public class AddTwoNumbers {
             if (j > 0) {
                 overflow = overflow + num2.get(--j);
             }
-            result.addFirst(overflow % 10);
+            result.add(0, overflow % 10);
             overflow = overflow / 10;
             if (i == 0 && j == 0 && overflow == 0) {
                 break;
